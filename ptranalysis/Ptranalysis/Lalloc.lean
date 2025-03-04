e529e1520385ecd67934c4b555a335e1c5dde474
Thundertonic <thundertonic@duck.com>
2025-03-04 18:46:43 -0500

    Snapshot in using Lean. Trying to switch to Coq.

--- ptranalysis/Ptranalysis/Lalloc.lean
+++ ptranalysis/Ptranalysis/Lalloc.lean
@@ -8,36 +8,17 @@ abbrev Addr := Nat
 abbrev Val := Addr
 abbrev AllocSite := Nat
 
-inductive PrimControl : Type where
-  | skip : PrimControl
-  | assign (vto : Var) (vfrom : Var) : PrimControl
-  | alloc (l: AllocSite) (vto : Var) : PrimControl
-  | seq (fst : PrimControl) (snd : PrimControl) : PrimControl
+inductive Control : Set AllocSite → Type where
+  | skip : Control ∅
+  | assign (vto : Var) (vfrom : Var) : Control ∅
+  | alloc (l : AllocSite) (vto : Var) : Control {l}
+  | seq {sfst ssnd : Set AllocSite} :
+      Disjoint sfst ssnd →
+      Control sfst →
+      Control ssnd →
+      Control (sfst ∪ ssnd)
 
-open PrimControl
-
-@[simp]
-def sitesOf : PrimControl → Set AllocSite
-  | skip => ∅
-  | assign _ _ => ∅
-  | alloc l _ => {l}
-  | seq fst snd => sitesOf fst ∪ sitesOf snd
-
-inductive WellFormed : PrimControl → Prop where
-  | skipWF : WellFormed skip
-  | assignWF {vto vfrom} : WellFormed (assign vto vfrom)
-  | allocWF {l vto} : WellFormed (alloc l vto)
-  | seqWF {fst snd} :
-      WellFormed fst →
-      WellFormed snd →
-      Disjoint (sitesOf fst) (sitesOf snd) →
-      WellFormed (seq fst snd)
-
-open WellFormed
-
-structure WFControl : Type where
-  prim : PrimControl
-  wf : WellFormed prim
+open Control
 
 structure HeapObj : Type where
   val : Option Val
@@ -50,12 +31,13 @@ structure State : Type where
   H : Heap
   σ : Stack
 
-abbrev StepState := WFControl × State
+structure StepState (site : Set AllocSite) : Type where
+  control : Control site
+  state : State
 
 @[simp]
 def emptyState : State := ⟨fun _ => none, fun _ => none⟩
 
-@[simp]
 def writeMap {β} (map : Var → β) (k : Var) (v : β) : Var → β :=
   fun k' => if k = k' then v else map k'
 
@@ -65,79 +47,92 @@ def unallocated (h : Heap) (addr : Addr) : Prop := h addr = none
 -- lemma stepPreservesWF : ∀ {c1 c2 c1' s s'},
 --   WellFormed c1 → Step (c1, s) (c1', s') → WellFormed c1'
 
--- All leaves are automatically well-formed
 @[simp]
-def skipWFControl : WFControl := ⟨skip, skipWF⟩
-@[simp]
-def assignWFControl (vto : Var) (vfrom : Var) : WFControl := ⟨assign vto vfrom, assignWF⟩
-@[simp]
-def allocWFControl (l : AllocSite) (vto : Var) : WFControl := ⟨alloc l vto, allocWF⟩
-
--- To construct a well-formed sequence, we need to supply a proof that the sites of the two sub-commands are disjoint
-@[simp]
-def wfToControl {c} (wf : WellFormed c) : WFControl := ⟨c, wf⟩
-
-lemma skipLSeqWF {c} (wf : WellFormed c) : WellFormed (seq skip c) := by
-  apply seqWF
-  . exact skipWF
-  . exact wf
-  . simp
+def skipLSeq {si} (c : Control si) : Control si := by
+  have d : Disjoint ∅ si := by simp
+  have exa : Control (∅ ∪ si) := seq d skip c
+  simp at exa
+  exact exa
 
 -- Small-step semantics of Lalloc
-inductive Step : StepState → StepState → Prop where
-  | seqL : ∀ {c1 c2 c1' : WFControl} {s s' : State},
-      (wf_c1c2 : WellFormed (seq c1.prim c2.prim)) →
-      (wf_c1'c2 : WellFormed (seq c1'.prim c2.prim)) →
-      Step (c1, s) (c1', s') →
-      Step (wfToControl wf_c1c2, s) (wfToControl wf_c1'c2, s')
-  | seqR : ∀ {c : WFControl} {s : State},
-      Step (wfToControl (skipLSeqWF c.wf), s) (c, s)
+inductive Step : ∀ {si1 si2}, StepState si1 → StepState si2 → Prop where
+  | seqL : ∀
+    {si1 si2 si1'}
+    {c1 : Control si1} {c2 : Control si2} {c1' : Control si1'}
+    {s s' : State},
+      (dis : Disjoint si1 si2) →
+      (dis' : Disjoint si1' si2) →
+      Step ⟨c1, s⟩ ⟨c1', s'⟩ →
+      Step ⟨seq dis c1 c2, s⟩ ⟨seq dis' c1' c2, s'⟩
+  | seqR : ∀ {si} {c : Control si} {s : State},
+      Step ⟨skipLSeq c, s⟩ ⟨c, s⟩
   | assign : ∀ {vto vfrom : Var} {h : Heap} {σ σ' : Stack},
       σ' = writeMap σ vto (σ vfrom) →
-      Step (assignWFControl vto vfrom, ⟨h, σ⟩) (skipWFControl, ⟨h, σ'⟩)
+      Step ⟨assign vto vfrom, ⟨h, σ⟩⟩ ⟨skip, ⟨h, σ'⟩⟩
   | alloc : ∀ {l : AllocSite} {addr : Addr} {vto : Var} {h h' : Heap} {σ σ' : Stack},
       unallocated h addr →
       h' = writeMap h addr (some ⟨none, l⟩) →
       σ' = writeMap σ vto addr →
-      Step (allocWFControl l vto, ⟨h, σ⟩) (skipWFControl, ⟨h', σ'⟩)
+      Step ⟨alloc l vto, ⟨h, σ⟩⟩ ⟨skip, ⟨h', σ'⟩⟩
 
-inductive StepClosure : StepState → StepState → Prop where
+inductive StepClosure : ∀ {si1 si2}, StepState si1 → StepState si2 → Prop where
   | refl : ∀ {st}, StepClosure st st
   | step : ∀ {st st' st''}, Step st st' → StepClosure st' st'' → StepClosure st st''
 
 -- # Concrete pointer analysis
 
-@[simp]
-def reachable (program : WFControl) (state : State) : Prop :=
-  ∃ program', StepClosure (program, emptyState) (program', state)
+def reachable {si} (program : Control si) (state : State) : Prop :=
+  ∃ (si' : Set AllocSite) (program' : Control si'), StepClosure ⟨program, emptyState⟩ ⟨program', state⟩
 
-@[simp]
-def concretePointsTo (program : WFControl) (v : Var) (site : AllocSite) : Prop :=
+def ConcretePointsTo {si} (program : Control si) (v : Var) (site : AllocSite) : Prop :=
   ∃ (S : State) (p : AllocSite),
-    reachable program S →
-    S.σ v = p ->
+    reachable program S ∧
+    S.σ v = p ∧
     match (S.H p) with
       | none => false
       | some hObj => hObj.id = site
 
 -- # Abstract pointer analysis
 
-abbrev PTAnalysis := WFControl → Var → AllocSite → Prop
+abbrev PTAnalysis := ∀ {si}, Control si → Var → AllocSite → Prop
 
 @[simp]
 def soundApprox (concrete : PTAnalysis) (abstract : PTAnalysis) : Prop :=
-  ∀ {p : WFControl} {v : Var} {site : AllocSite},
+  ∀ {si} {p : Control si} {v : Var} {site : AllocSite},
     concrete p v site -> abstract p v site
 
 infix:51 " ≼ " => soundApprox
 
--- ## Andersen-style points-to analysis
+-- ## Generally useful relations
 
-def PTAlloc (p : WFControl) (var : Var) (site : AllocSite) : Prop :=
-  match p.prim with
+def PTAlloc {si} (p : Control si) (var : Var) (site : AllocSite) : Prop :=
+  match p with
   | alloc l vto => vto = var ∧ l = site
-  | seq fst snd => PTAlloc (wfToControl fst.wf) var site ∨ PTAlloc (wfToControl snd.wf) var site
+  | seq _ fst snd => PTAlloc fst var site ∨ PTAlloc snd var site
   | _ => false
 
+def PTMove {si} (p : Control si) (vto vfrom : Var) : Prop :=
+  match p with
+  | assign vto' vfrom' => vto = vto' ∧ vfrom = vfrom'
+  | seq _ fst snd => PTMove fst vto vfrom ∨ PTMove snd vto vfrom
+  | _ => false
+
+-- ## Andersen-style points-to analysis
+
+-- This is based on the Andersen-style points-to analysis from Smaragdakis et al.
+-- Any rules related to inter-procedure and field sensitivity are omitted for Lalloc
 inductive AndersenPointsTo : PTAnalysis where
-  | alloc : ∀ {var : Var} {site : AllocSite}
+  | alloc : ∀ {si} {var : Var} {site : AllocSite} {p : Control si},
+      PTAlloc p var site → AndersenPointsTo p var site
+  | move : ∀ {si} {vto vfrom : Var} {site : AllocSite} {p : Control si},
+      PTMove p vto vfrom → AndersenPointsTo p vfrom site → AndersenPointsTo p vto site
+
+theorem andersenSound : ConcretePointsTo ≼ AndersenPointsTo := by
+  simp; intro si p v site con
+  induction p with
+  | skip =>
+    have ncon : ¬ (ConcretePointsTo skip v site) := by
+      simp [ConcretePointsTo]
+      intro s reach
+      simp [reachable] at reach
+      
